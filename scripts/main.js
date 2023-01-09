87aa40049c06936a2b1abc3bce5cbf71fdb45eb4
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-01-09 12:29:53 -0700

    edits before class

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 let date = new Date();
 let year = date.getFullYear();
-document.querySelector('h3').innerHTML = "&copy" + year;
+document.querySelector('h3').innerHTML = "&copy" + year + " | Megan Bateman | Idaho";
 
 let currentdate = document.lastModified;
 document.querySelector('h4').textContent = `Last Updated: ${currentdate}`;
