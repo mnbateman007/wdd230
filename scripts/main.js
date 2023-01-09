62d7254a7aa3840306087712a8d02ed17c15e1d1
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-01-09 11:27:19 -0700

    Edits for 9Jan

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,5 @@ let year = date.getFullYear();
 document.querySelector('h3').innerHTML = "&copy" + year;
 
 let currentdate = document.lastModified;
-document.querySelector('div').textContent = `Last Updated: ${currentdate}`;
+document.querySelector('h4').textContent = `Last Updated: ${currentdate}`;
 
