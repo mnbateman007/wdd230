dd67f3405119af85a3e9cbbc5c1a0dbe44a6048c
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-01-29 16:47:12 -0700

    grid

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,6 @@
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+})
