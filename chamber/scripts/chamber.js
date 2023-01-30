e7c8a2bafba41b23c83364953dfef654f27a4927
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-01-30 12:06:08 -0700

    lots of stuff

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -4,3 +4,10 @@ const hambutton = document.querySelector('.ham');
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive');
 })
+
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector('h3').innerHTML = "&copy" + year + " | Megan Bateman | Idaho";
+
+let currentdate = document.lastModified;
+document.querySelector('h4').textContent = `Last Updated: ${currentdate}`;
