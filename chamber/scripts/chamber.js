0c81f64fbad7be2cde7c9e3dc06fbdf4c21afaf5
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-02-08 13:28:39 -0700

    windchill

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -39,3 +39,53 @@ if (day == 1 || day == 2) {
     let banner = document.querySelector('.banner');
     banner.style.display = 'none';
 }
+
+
+
+
+// function toggleMenu() {
+//     document.getElementById("primaryNav").classList.toggle("open");
+//     document.getElementById("hamburgerBtn").classList.toggle("open");
+
+// }
+
+// const x = document.getElementById('hamburgerBtn')
+// x.onclick = toggleMenu;
+
+// let date = new Date();
+// let day = date.getDay();
+// let year = date.getFullYear();
+
+// document.querySelector('#copy').innerHTML = '&copy;' + year + "| Idaho Falls Chamber";
+
+// let currentdate = document.lastModified;
+
+// document.querySelector('#year').textContent = `Last Updated: ${currentdate}`;
+
+// const datefield = document.querySelector(".date");
+// const now = new Date();
+// const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+// 	now
+// );
+
+// datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// if (day == 1 || day == 2){
+//     document.querySelector('#banner').innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+// }
+
+
+// winds chill calculation
+
+const temp = document.querySelector('.t').textContent;
+const ws = document.querySelector('.ws').textContent;
+
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
+
+// console.log(chill);
+
+
+if(temp <= 50 && ws >= 3) {
+    document.querySelector('.wc').textContent = chill;
+}
+
