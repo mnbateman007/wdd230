98aeaec5d7db7af2edb849ff638c555d7a7ed5d4
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-02-13 08:39:17 -0700

    css changes

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,9 +1,11 @@
-// const mainnav = document.querySelector('.navigation');
-// const hambutton = document.querySelector('.ham');
 
-// hambutton.addEventListener('click', () => {
-//     mainnav.classList.toggle('responsive');
-// })
+
+// // const mainnav = document.querySelector('.navigation');
+// // const hambutton = document.querySelector('.ham');
+
+// // hambutton.addEventListener('click', () => {
+// //     mainnav.classList.toggle('responsive');
+// // })
 function toggleMenu() {
     document.querySelector("#mainview").classList.toggle("open");
     document.querySelector("#hamburgerBtn").classList.toggle("open");
@@ -11,10 +13,10 @@ function toggleMenu() {
 const x = document.querySelector("#hamburgerBtn")
 x.onclick = toggleMenu;
 
-// const hambutton = document.querySelector('.ham');
-// const mainnav = document.querySelector('.navigation')
+// // const hambutton = document.querySelector('.ham');
+// // const mainnav = document.querySelector('.navigation')
 
-// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+// // hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 let date = new Date();
 let day = date.getDay();
@@ -26,75 +28,92 @@ let currentdate = document.lastModified;
 
 document.querySelector('year').textContent = `Last Updated: ${currentdate}`;
 
-
-// let date = new Date;
-
-// get weekDay = date.getDay();
-
-// if (weekDay ==1 || weekDay ==2){
-    
-// }
-
-let day = date.getDay();
-
-if (day == 1 || day == 2) {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'block';
-
-    document.querySelector(".banner-button").addEventListener("click", function () {
-        banner.style.display = 'none';
-      });
-} else {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'none';
-}
-
-
-
-
-// function toggleMenu() {
-//     document.getElementById("primaryNav").classList.toggle("open");
-//     document.getElementById("hamburgerBtn").classList.toggle("open");
-
-// }
-
-// const x = document.getElementById('hamburgerBtn')
-// x.onclick = toggleMenu;
-
-// let date = new Date();
-// let day = date.getDay();
-// let year = date.getFullYear();
-
-// document.querySelector('#copy').innerHTML = '&copy;' + year + "| Idaho Falls Chamber";
-
-// let currentdate = document.lastModified;
-
-// document.querySelector('#year').textContent = `Last Updated: ${currentdate}`;
-
 // const datefield = document.querySelector(".date");
-// const now = new Date();
-// const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-// 	now
-// );
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
 
-// datefield.innerHTML = `<em>${fulldate}</em>`;
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
-// if (day == 1 || day == 2){
-//     document.querySelector('#banner').innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+if (day == 1 || day == 2){
+    document.querySelector('#banner').innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+}
+
+// // let date = new Date;
+
+// // get weekDay = date.getDay();
+
+// // if (weekDay ==1 || weekDay ==2){
+    
+// // }
+
+// let day = date.getDay();
+
+// if (day == 1 || day == 2) {
+//     let banner = document.querySelector('.banner');
+//     banner.style.display = 'block';
+
+//     document.querySelector(".banner-button").addEventListener("click", function () {
+//         banner.style.display = 'none';
+//       });
+// } else {
+//     let banner = document.querySelector('.banner');
+//     banner.style.display = 'none';
 // }
 
 
-// winds chill calculation
-
-const temp = document.querySelector('.t').textContent;
-const ws = document.querySelector('.ws').textContent;
-
-let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
-
-// console.log(chill);
 
 
-if(temp <= 50 && ws >= 3) {
-    document.querySelector('.wc').textContent = chill;
-}
+// // function toggleMenu() {
+// //     document.getElementById("primaryNav").classList.toggle("open");
+// //     document.getElementById("hamburgerBtn").classList.toggle("open");
+
+// // }
+
+// // const x = document.getElementById('hamburgerBtn')
+// // x.onclick = toggleMenu;
+
+// // let date = new Date();
+// // let day = date.getDay();
+// // let year = date.getFullYear();
+
+// // document.querySelector('#copy').innerHTML = '&copy;' + year + "| Idaho Falls Chamber";
+
+// // let currentdate = document.lastModified;
+
+// // document.querySelector('#year').textContent = `Last Updated: ${currentdate}`;
+
+// // const datefield = document.querySelector(".date");
+// // const now = new Date();
+// // const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+// // 	now
+// // );
+
+// // datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// // if (day == 1 || day == 2){
+// //     document.querySelector('#banner').innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+// // }
+
+
+// // winds chill calculation
+
+// // const temp = document.querySelector('.t').textContent;
+// // const ws = document.querySelector('.ws').textContent;
+
+// // let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
+
+// // console.log(chill);
+
+
+
+// // let windchill = document.querySelector('.wc');
+// // if (temp <= 50 && ws >=3) {
+// //     windchill.textContent = chill;
+// // }
+
+// // if(temp <= 50 && ws >= 3) {
+// //     document.querySelector('.wc').textContent = chill;
+// // }
 
