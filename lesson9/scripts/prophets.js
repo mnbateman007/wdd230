908b86715eb328da38b063d30909a9bf1458c004
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-03-06 11:32:03 -0700

    json

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -6,6 +6,7 @@ async function getProphetData() {
     const data = await response.json();
     console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
     displayProphets(data.prophets);
+    displayTable(data.prophets);
 }
 
   getProphetData();
@@ -37,3 +38,63 @@ async function getProphetData() {
     } // end of forEach loop
   )} // end of function expression
 
+
+/*Table Function*/
+
+function displayTable(prophets){
+
+  // let list = document.getElementById("list").style.display = "none";
+  // list.addEventListener("click", displayTable);
+
+  // let cards = document.getElementById("cards").style.display = "none";
+  // cards.addEventListener("click",displayProphets);
+    // let row = document.querySelectorAll('tr');
+    //     row.forEach((item)=>{
+    //       item.remove();
+    //     })
+    // let section = document.querySelectorAll('tr');
+    //     section.forEach((item)=>{
+    //       item.remove();
+    //     })
+
+   
+
+  prophets.forEach((prophet)=>{
+    let tr = document.createElement('tr');
+    let td_name = document.createElement('td');
+    let td_birthplace = document.createElement('td');
+    let td_birthdate = document.createElement('td');
+
+    td_name.textContent = `${prophet.name} ${prophet.lastname}`;
+    td_birthplace.textContent = prophet.birthplace;
+    td_birthdate.textContent = prophet.birthdate;
+
+    tr.appendChild(td_name);
+    tr.appendChild(td_birthplace);
+    tr.appendChild(td_birthdate);
+
+    document.querySelector('table').appendChild(tr);
+
+  })
+}
+
+table_class = document.querySelector(".table")
+card_class =document.querySelector(".cards")
+table_button = document.querySelector("#list")
+card_button =document.querySelector("#cards")
+
+
+
+
+card_button.addEventListener("click", () => {
+  card_class.style.display = "flex";
+  table_class.style.display = "none";
+});
+
+table_button.addEventListener("click", () => {
+  card_class.style.display = "none";
+  table_class.style.display = "block";
+});
+
+// let lists = document.getElementById("lists");
+// lists.addEventListener("click",displayTable);
