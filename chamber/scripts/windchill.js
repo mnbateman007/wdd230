734adb17bd6fb81f2e9212ccfe7166fba5263712
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-03-08 13:45:10 -0700

    portal

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,16 @@
-const temp = document.querySelector('.t').textContent;
-const ws = document.querySelector('.ws').textContent;
+// const temp = document.querySelector('#t').textContent;
+// const ws = document.querySelector('#ws').textContent;
 
-let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
+// let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(ws,0.16)) + (0.4275*temp*Math.pow(ws,0.16)));
 
-console.log(chill);
+// console.log(chill);
 
 
 
-let windchill = document.querySelector('.wc');
-if (temp <= 50 && ws >=3) {
-    windchill.textContent = chill;
-}
+// let windchill = document.querySelector('.wc');
+// if (temp <= 50 && ws >=3) {
+//     windchill.textContent = chill;
+// }
 
 
 // storage local and session
