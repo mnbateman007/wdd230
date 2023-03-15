bfb67a6ec71c820001f3fee90cd5663093171420
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-03-15 13:36:02 -0600

    current

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -42,6 +42,42 @@ if (day == 1 || day == 2){
 // }
 
 
+// function toggleMenu() {
+//     document.getElementById("primaryNav").classList.toggle("open");
+//     document.getElementById("hamburgerBtn").classList.toggle("open");
+
+// }
+
+// const x = document.getElementById('hamburgerBtn')
+// x.onclick = toggleMenu;
+
+// let date = new Date();
+// let day = date.getDay();
+// let year = date.getFullYear();
+
+// document.querySelector('#copy').innerHTML = '&copy;' + year + "| Idaho Falls Chamber";
+
+// let currentdate = document.lastModified;
+
+// document.querySelector('#year').textContent = `Last Updated: ${currentdate}`;
+
+// const datefield = document.querySelector(".date");
+// const now = new Date();
+// const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+// 	now
+// );
+
+// datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// if (day == 1 || day == 2){
+//     document.querySelector('#banner').innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+// }
+
+
+// // storage local and session
+// localStorage.setItem('name','Adelyn Smith - Permanent');
+// sessionStorage.setItem('class','WDD230 - not Permanent');
+
 
 
 
