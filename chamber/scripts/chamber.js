fc00775b41bb9bce077a604c72d47def347bca75
mnbateman007@gmail.com <mnbateman007@gmail.com>
2023-03-29 11:46:57 -0600

    try again

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -25,25 +25,3 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 
 localStorage.setItem('name','Megan Bateman - Permanent');
 sessionStorage.setItem('class','WDD230 - not Permanent');
-
-// let day = date.getDay();
-
-// if(!localStorage.getItem('lastvisit')) {
-//     localStorage.setItem('lastvisit', Date.now());
-//     document.getElementById('visits').textContent = 'This is your 1st visit';
-// } else {
-//     giveDate();
-// }
-
-// function giveDate() {
-//     let lastDate = localStorage.getItem('lastvisit');
-//     let currDate = Date.now();
-    
-//     let difference = currDate - lastDate;
-//         console.log(difference);
-//         let daysDifference = Math.floor(difference/1000/60/60/24);
-
-//     document.getElementById('visits').textContent = daysDifference;
-
-//     localStorage.setItem('lastvisit', Date.now());
-    
--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -20,11 +20,8 @@ const io = new IntersectionObserver(
                 preloadImage(entry.target);
                 io.unobserve(entry.target);
             }
-            
             console.log(entries);
         })
-
-
     },
     options
 );
@@ -43,7 +40,6 @@ if(!localStorage.getItem('lastvisit')) {
 function giveDate() {
     let lastDate = localStorage.getItem('lastvisit');
     let currDate = Date.now();
-    
     let difference = currDate - lastDate;
         console.log(difference);
         let daysDifference = Math.floor(difference/1000/60/60/24);
@@ -51,141 +47,4 @@ function giveDate() {
     document.getElementById('visits').textContent = daysDifference;
 
     localStorage.setItem('lastvisit', Date.now());
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// const images = document.querySelectorAll('[data-src]');
-
-// const options = {
-//     threshold: .5};
-
-// function preloadImage(img){
-//     const source = img.getAttribute('data-src');
-//     if(!source){
-//         return;
-//     }
-//     img.src = source;
-// }
-
-// const io = new IntersectionObserver(
-//     (entries)=>{
-//         entries.forEach(entry => {
-//             if(!entry.isIntersecting) {
-//                 return;
-//             } else{
-//                 preloadImage(entry.target);
-//                 io.unobserve(entry.target);
-//             }
-            
-//             console.log(entries);
-//         })
-
-
-//     },
-//     options
-// );
-
-// images.forEach(img => {
-//     io.observe(img);
-// })
-
-// if(!localStorage.getItem('lastvisit')) {
-//     localStorage.setItem('lastvisit', Date.now());
-//     document.getElementById('visits').textContent = 'This is your 1st visit';
-// } else {
-//     giveDate();
-// }
-
-// function giveDate() {
-//     let lastDate = localStorage.getItem('lastvisit');
-//     let currDate = Date.now();
-    
-//     let difference = currDate - lastDate;
-//         console.log(difference);
-//         let daysDifference = Math.floor(difference/1000/60/60/24);
-
-//     document.getElementById('visits').textContent = daysDifference;
-
-//     localStorage.setItem('lastvisit', Date.now());
-
-// }
-
-// const images = document.querySelectorAll('[data-src]');
-
-// const options = { threshold: .5};
-
-// function preloadImg(img) {
-//     const source = img.getAttribute('data-src');
-//     if(!source){
-//         return;
-//     }
-//     img.src = source;
-// }
-
-// const io = new IntersectionObserver (
-//     (entries,io)=> {
-//         entries.forEach(entry => {
-//             if(!entry.isIntersecting){
-//                 return;
-//             } else {
-//                 preloadImg(entry.target);
-//                 io.unobserve(entry.target);
-//             }
-//             console.log(entries);
-//         })
-
-//     },
-//     options
-// );
-
-// images.forEach(img => {
-//     io.observe(img);
-// })
-
-// const visitsDisplay = document.querySelector("#visit");
-
-// let numVisits = Number(window.localStorage.getItem("visits-ls"));
-
-// if (numVisits !== 0) {
-// 	visitsDisplay.textContent = numVisits;
-// } else {
-// 	visitsDisplay.textContent = `This is your first visit!`;
-// }
-
-// numVisits++;
-
-// localStorage.setItem("visits-ls", numVisits);
-
-
-
-// todayDisplay.textContent = Date.now();
-
-// if(!localStorage.getItem('lastvisit')) {
-//     localStorage.setItem('lastvisit', Date.now());
-//     document.getElementById('visits').textContent = 'This is your 1st visit';
-// } else {
-//     giveDate();
-// }
-
-// function giveDate() {
-//     let lastDate = localStorage.getItem('lastvisit');
-//     let currDate = Date.now();
-    
-//     let difference = currDate - lastDate;
-//         console.log(difference);
-//         let daysDifference = Math.floor(difference/1000/60/60/24);
-
-//     document.getElementById('visits').textContent = daysDifference;
-
-//     localStorage.setItem('lastvisit', Date.now());
